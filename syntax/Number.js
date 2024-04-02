e655419ebabc1ed54ffd32148fe03331b4ca0b62
장도훈 <jehgns1@gmail.com>
2024-04-02 10:43:04 +0900

    first

---
+++ syntax/Number.js
@@ -0,0 +1,6 @@
+//기본적인 javascript문법!!
+
+console.log(1+1);   //2
+console.log(3-1);   //2
+console.log(3*3);   //9
+console.log(12/3);  //4
---
+++ syntax/string.js
@@ -0,0 +1,5 @@
+console.log(1+1);  //2
+console.log('1'+'1');  //11 
+console.log(' Finders keepers, loser weepers. Every dog has his day. Even a worm will turn.'.length);  //78
+
+
